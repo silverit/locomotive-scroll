76e138c3e7c2954c819db6c31e50cfea618b00d0
Phuong Vo <vlaphuong1991@gmail.com>
2023-04-02 22:30:47 +0700

    update

--- src/scripts/utils/transform.js
+++ src/scripts/utils/transform.js
@@ -11,12 +11,12 @@ export function getTranslate(el) {
     const style = getComputedStyle(el);
     const transform = style.transform || style.webkitTransform || style.mozTransform;
 
-    let mat = transform.match(/^matrix3d\((.+)\)$/);
+    let mat = transform?.match(/^matrix3d\((.+)\)$/);
     if (mat) {
         translate.x = mat ? parseFloat(mat[1].split(', ')[12]) : 0;
         translate.y = mat ? parseFloat(mat[1].split(', ')[13]) : 0;
     } else {
-        mat = transform.match(/^matrix\((.+)\)$/);
+        mat = transform?.match(/^matrix\((.+)\)$/);
         translate.x = mat ? parseFloat(mat[1].split(', ')[4]) : 0;
         translate.y = mat ? parseFloat(mat[1].split(', ')[5]) : 0;
     }
